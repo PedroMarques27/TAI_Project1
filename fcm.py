5a1b94fc4383c73df03011b2c480f850356c891b
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-10-29 16:47:28 +0100

    Added Substring formation

--- fcm.py
+++ fcm.py
@@ -1,9 +1,27 @@
-def getWordsFromFile(file):
-    f  = open(file, 'r')
-    data = f.readlines()
-    wordArray = []
-    for line in data:
-        wordArray += line.split()
-    print(wordArray)
+def getWordsFromFile(data,k):
+    
+    dataset = []
+    i = 0
+    while i<=len(data):
+        word = data[i:i+k]
+        i+=1
+        dataset.append(word)
+    return set(dataset)         #gets all possible character sequences with length k
 
-getWordsFromFile('bible.txt')
+
+def find_all(data, substring):
+    start = 0
+    occurences = []
+    while True:
+        start = data.find(substring, start)
+        if start == -1: return
+        start += len(substring)
+        occurences+=[start]     #gets all index where substring starts
+
+def init(file, k, alpha):
+    f  = open(file, 'r')
+    data = f.read()
+    dataset = getWordsFromFile(data,k)
+    
+
+getWordsFromFile('bible.txt',3)
