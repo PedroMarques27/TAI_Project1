55d7e6fc292261b33c1b4f9b3fb634d4891db534
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-10-30 20:08:42 +0100

    Saving ovvurences

--- fcm.py
+++ fcm.py
@@ -1,12 +1,14 @@
 def getWordsFromFile(data,k):
-    
+    characters = []
     dataset = []
     i = 0
     while i<=len(data):
         word = data[i:i+k]
         i+=1
-        dataset.append(word)
-    return set(dataset)         #gets all possible character sequences with length k
+        dataset.append(word.lower())
+        for char in word:
+            characters.append(char.lower())
+    return set(dataset), set(characters)         #gets all possible character sequences with length k
 
 
 def find_all(data, substring):
@@ -14,14 +16,35 @@ def find_all(data, substring):
     occurences = []
     while True:
         start = data.find(substring, start)
-        if start == -1: return
-        start += len(substring)
-        occurences+=[start]     #gets all index where substring starts
+
+        if start == -1 or start >= len(data): return occurences
+        occurences+=[start]
+        start+=len(substring)
+        
+    print(occurences)   #gets all index where substring starts
 
 def init(file, k, alpha):
     f  = open(file, 'r')
     data = f.read()
-    dataset = getWordsFromFile(data,k)
-    
+    dataset, chars = getWordsFromFile(data,k)
+  
+    count(chars, dataset, data, k)
 
-getWordsFromFile('bible.txt',3)
+def count(universe, dataset, data,k):
+    for word in dataset:
+        if len(str(word))>0: 
+            counting = dict.fromkeys(universe, 0)
+            occ = find_all(data, word)
+            for o in occ:
+                if o+k<len(data):
+                    counting[data[o+k].lower()]+=1
+            writeToFile(counting, word)
+       
+def writeToFile( data, substring):
+    f = open("output.txt", "a")
+    substring = substring.replace("\n", "\\n")
+    f.write(substring + "\n")
+    f.write(str(data)+"\n")
+    f.close()
+        
+init('bible.txt',3,2)
