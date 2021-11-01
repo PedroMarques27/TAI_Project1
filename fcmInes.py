3ce605364b108c9467aa9b2824e8d8102d5c50ca
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-11-01 14:33:19 +0000

    Adicionadas funcoes de probabilidade

--- fcmInes.py
+++ fcmInes.py
@@ -1,18 +1,19 @@
-import re
+import re, copy
 from typing import Text
 
 def getExpressions(data, k):
     # remove all but words and spaces
-    cleanData = re.sub(r'[^a-zA-Z ]+', '', data)
+    cleanData = re.sub(r'[^a-zA-Z ]+', '', data).lower()
     cleanData = re.sub("\s\s+", " ", cleanData)
     
     expressionsDic = {}
+    alphabet = list(set(cleanData))
 
     # pass through all the characters and insert every 3 in the dict 
     # along with the next character and the times they appear
-    i = 0
+    
     # TODO adicionar as letras finais
-    while i<=(len(cleanData)-k-1):
+    for i in range(len(cleanData)-k):
         expression = cleanData[i:i+k]
         
         if expression in expressionsDic.keys():
@@ -21,13 +22,29 @@ def getExpressions(data, k):
             else:
                 expressionsDic[expression][cleanData[i+k]] = 1   
         else:
-            expressionsDic[expression] = {}
+            expressionsDic[expression] = dict.fromkeys(alphabet, 0)
             expressionsDic[expression][cleanData[i+k]] = 1
-        i+=1
+        
     
     # print(str(expressionsDic))
+
     return expressionsDic
 
+
+def calculateProbability(dictionary, a):
+    probabilities = copy.deepcopy(dictionary)
+    for expression in dictionary:
+        total = 0;
+        for char in dictionary[expression]:
+            total+=dictionary[expression][char]
+
+        for char in dictionary[expression]:
+            occurences = dictionary[expression][char]
+     
+            probabilities[expression][char] = (occurences + a) / (total + (a * len(probabilities[expression])))
+
+    
+    
 def generateText(expressionsDic, k):
     text = list(expressionsDic.keys())[0]
     
@@ -43,12 +60,21 @@ def generateText(expressionsDic, k):
 
     return text
 
+def writeToFile( data, substring):
+    f = open("output.txt", "w")
+    substring = str(substring).replace("\n", "\\n")
+    f.write(substring + "\n")
+    f.write(str(data)+"\n")
+    f.close()
+        
 def init(filename, k):
     f  = open(filename, 'r')
     data = f.read()
     expressions_dict = getExpressions(data,k)
+    calculateProbability(expressions_dict, 0.01)
+
     text = generateText(expressions_dict, k)
     print("TEXT " + text)
     
 
-init("sherlock.txt", 15)
+init("sherlock.txt", 3)
