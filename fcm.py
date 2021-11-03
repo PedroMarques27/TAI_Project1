a5da47eec300dfddb0ac9e458a46208ee6d213a2
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-11-03 23:43:16 +0000

    Fixed implementation of dictionary

--- fcm.py
+++ fcm.py
@@ -2,8 +2,9 @@ import math
 import re, copy
 from typing import Text
 import sys
+import time
 from random import random
-
+from pathlib import Path
 
 class FcmClass:
 
@@ -27,6 +28,7 @@ class FcmClass:
         return self.alphabet
 
     def get_expressions_from_data(self):
+
         # pass through all the characters and insert every 3 in the dict 
         # along with the next character and the times they appear
         for i in range(len(self.data) - self.k):
@@ -34,15 +36,16 @@ class FcmClass:
             next_char = self.data[i + self.k]
 
             if type(self.table) == dict:
-                if context in self.table:
-                    if next_char in self.table[context]:
-                        self.table[context][next_char] += 1
-                    else:
-                        self.table[context][next_char] = 1
-                else:
+                if context not in self.table:
                     self.table[context] = {next_char: 1}
+                else:
+                    if next_char not in self.table[context]:
+                        self.table[context][next_char] = 1
+                    else:
+                        self.table[context][next_char] += 1
             else:
                 self.table[self.get_index(context)][self.alphabet.index(next_char)] += 1
+
         return self.table
 
     def get_index(self, context):
@@ -62,48 +65,48 @@ class FcmClass:
         return (a_size ** self.k) * a_size * 16 / 8 / 1024 / 1024
 
     def get_probability_table(self):
-        self.probabilities_table = copy.deepcopy(self.table)
+        self.entropy = 0
+        total_sequences = len(self.data) - self.k
+
         if type(self.table) == dict:
+            self.probabilities_table = {}
             for context in self.table:
                 total = sum(self.table[context].values())
                 for char in self.table[context]:
                     occurrence = self.table[context][char]
-                    self.probabilities_table[context][char] = self.get_probability(occurrence, total)
+                    if context not in self.probabilities_table:
+                        self.probabilities_table[context] = {}
+                    self.probabilities_table[context][char] = (occurrence + self.a) / (total + (self.a * len(self.alphabet)))
+                ent_row = -sum([x * math.log2(x) for x in self.probabilities_table[context].values()])
+                self.entropy += (total / total_sequences) * ent_row
+
         else:
-            for x in range(len(self.alphabet) ** self.k):
-                total = sum(self.table[x])
-                self.probabilities_table[x] = [self.get_probability(occurrence, total) for occurrence in self.table[x]]
+            self.probabilities_table = []
+            for row in self.table:
+                total = sum(row)
+                prob_row = [(occurrence + self.a) / (total + (self.a * len(self.alphabet))) for occurrence in row]
+                self.probabilities_table.append(prob_row)
 
-    def get_probability(self, occurrence, total):
-        return (occurrence + self.a) / (total + (self.a * len(self.alphabet)))
+                ent_row = -sum([x*math.log2(x) for x in prob_row])
+                self.entropy += (total / total_sequences) * ent_row
 
-    def get_table_entropy(self):
 
-        total_entropy = 0
-        total_sequences = len(self.data)-self.k
-        if type(self.table) == dict:
-            for i in range(len(self.probabilities_table)):
-                total = sum(self.table[i].values())
-                ent_row = -sum([x * math.log2(x) for x in self.probabilities_table[i].values()])
-                total_entropy += (total / total_sequences) * ent_row
-        else:
-            for i in range(len(self.probabilities_table)):
-                total = sum(self.table[i])
-                ent_row = -sum([x * math.log2(x) for x in self.probabilities_table[i]])
-                total_entropy += (total / total_sequences) * ent_row
-
-        return total_entropy
 
     def get_next_char(self, context):
         selected_char = random()
         initial_value = 0
         if type(self.table) == dict:
             if context not in self.probabilities_table:
-                self.probabilities_table[context] = dict.fromkeys(self.alphabet, self.a / (self.a * len(self.alphabet)))
-            for char in sorted(self.probabilities_table[context]):
-                if initial_value <= selected_char < initial_value + self.probabilities_table[context][char]:
+                self.probabilities_table[context]={}
+            for char in self.alphabet:
+                if char in self.probabilities_table[context]:
+                    prob = self.probabilities_table[context][char]
+                else:
+                    prob = self.a / (self.a * len(self.alphabet))
+
+                if initial_value <= selected_char < initial_value + prob:
                     return char
-                initial_value += self.probabilities_table[context][char]
+                initial_value += prob
         else:
             index = self.get_index(context)
             for i in range(len(self.alphabet)):
@@ -112,22 +115,14 @@ class FcmClass:
                 initial_value += self.probabilities_table[index][i]
 
     def __init__(self, filename="sherlock.txt", a=0.1, k=1):
-        f = open(filename, 'r')
+        self.data = Path(filename).read_text()
         self.expressions = []
         self.table = []
         self.probabilities_table = []
         self.alphabet = []
         self.a = a
         self.k = k
-        self.data = f.read()
         self.get_normalized_string()
         self.get_alphabet()
         self.make_table()
 
-fcm = FcmClass("sherlock.txt", 0.1, 3)
-fcm.get_expressions_from_data()
-print('Finished Reading File...')
-fcm.get_probability_table()
-print('Finished calculating probabilities...')
-print('Total entropy: ' + str(fcm.get_table_entropy()))
-print('Generating text...')
--- generator.py
+++ generator.py
@@ -1,33 +1,33 @@
 import sys
 
 from fcm import FcmClass
+import time
 import random
 from random import randint
 
+
+def get_initial_sequence(alphabet, k):
+    sequence = ""
+
+    for i in range(0, k):
+        sequence += random.choice(alphabet)
+
+    return sequence
 def generator(filename, alpha, k):
     fcm = FcmClass(filename, alpha,k)
     fcm.get_expressions_from_data()
-    print('Finished Reading File...')
     fcm.get_probability_table()
-    print('Finished calculating probabilities...')
-    print('Total entropy: ' + str(fcm.get_table_entropy()))
-    print('Generating text...')
-    if type(fcm.table) == dict:
-        value = randint(0, len(fcm.table))
-        text = list(fcm.table.keys())[value]
+    print('Total entropy: ' + str(fcm.entropy))
 
-    else:
-        value = randint(0, len(fcm.expressions))
-        text = fcm.expressions[value]
+    text = get_initial_sequence(fcm.alphabet, k)
 
 
     i = 0
-    while i < 10000:
+    while i < 1000:
         i += 1
         lastKcharacters = text[-k:]
         text+=fcm.get_next_char(lastKcharacters)
     writeToFile(text)
-    print(text)
 
 def writeToFile(string):
     f = open("output.txt", "w", encoding='utf-8')
@@ -47,7 +47,26 @@ def main():
 
     generator(file_name, k, alpha)
     '''
-
+    print('-'*22)
+    starttime = time.time()
     generator("sherlock.txt", 0.1, 3)
+    print('Time:' + str(time.time() - starttime)+"s")
+    print('-'*22)
+    starttime = time.time()
+    generator("sherlock.txt", 0.1, 4)
+    print('Time:' + str(time.time() - starttime)+"s")
+    print('-'*22)
+    starttime = time.time()
+    generator("sherlock.txt", 0.1, 5)
+    print('Time:' + str(time.time() - starttime)+"s")
+    print('-'*22)
+    starttime = time.time()
+    generator("sherlock.txt", 0.1, 6)
+    print('Time:' + str(time.time() - starttime)+"s")
+    print('-'*22)
+    starttime = time.time()
+    generator("sherlock.txt", 0.1, 7)
+    print('Time:' + str(time.time() - starttime)+"s")
+    print('-'*22)
 if __name__== "__main__":
     main()
